9dd614c8520ea0086ceb9e5dd6a1b2f3ae512b75
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-07-20 21:54:33 +0900

    template

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,15 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TodosComponent } from './todos/todos.component';
-import { TodoComponent } from './todos/todo/todo.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { TodosComponent } from "./todos/todos.component";
+import { TodoComponent } from "./todos/todo/todo.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [TodosComponent, TodoComponent],
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule, FormsModule],
   exports: [TodosComponent]
 })
-export class TodoModule { }
+export class TodoModule {}
--- src/app/todo/todos/todos.component.ts
+++ src/app/todo/todos/todos.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-todos',
-  templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
+  selector: "app-todos",
+  templateUrl: "./todos.component.html",
+  styleUrls: ["./todos.component.scss"]
 })
 export class TodosComponent implements OnInit {
+  newText = "";
+  todos: {
+    done: boolean;
+    text: string;
+  }[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.todos = [
+      { done: false, text: "운동하기" },
+      { done: true, text: "공부하기" }
+    ];
   }
 
+  toggleTodo(todo: any) {
+    todo.done = !todo.done;
+  }
+
+  addTodo(newText: string) {
+    this.todos.push({
+      done: false,
+      text: newText
+    });
+    this.newText = "";
+  }
+
+  ngOnInit() {}
 }
